2d04fb08b4dc9cf3f9c3737a4b79fe989c70a490
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-08-05 14:30:15 +0200

    Map done, bug with the display of the marker needs to be solved yet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'LeafletAngularMaps';
+  
+  map!:L.Map;
+
+  constructor(){}
+  
+  ngAfterViewInit():void{
+    this.createMap();
+    this.addHomeMarker();
+  }
+
+  createMap(){
+
+    const parcThabor={
+      lat: 48.114384,
+      lng: 1.669494
+    }
+
+    const zoomLevel= 10;
+
+    this.map= new L.Map('map', {
+      center: [parcThabor.lat, parcThabor.lng],
+      zoom: zoomLevel
+    })
+
+    const mainLayer=L.tileLayer(
+          'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          {
+            minZoom: 1,
+            maxZoom: 17,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          }
+    );
+    mainLayer.addTo(this.map)
+  }
+
+  addHomeMarker(){
+    const homeMarker=L.marker({lat:39.46975, lng: -0.37739});
+    homeMarker.bindPopup('This is Valencia',{
+      closeButton: true
+    })
+    homeMarker.addTo(this.map);
+
+    L.circle({lat: 39.46975, lng: -0.37739}, {
+      color: 'steelblue',
+      radius: 500,
+      fillColor: 'steelblue',
+      opacity: -0.5, 
+    }).addTo(this.map);
+  }
 }
